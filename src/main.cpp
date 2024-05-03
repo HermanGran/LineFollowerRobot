2b8efafc22ff58d056d790e390b8c838837633dd
HermanGran <142888421+HermanGran@users.noreply.github.com>
2024-05-03 12:38:35 +0200

    Finished competition, last commit for now

--- src/main.cpp
+++ src/main.cpp
@@ -26,21 +26,23 @@ RobotOdometry odometry(14.5);
 StateMachine stateMachine(pid, motorA, motorB, odometry, button);
 
 // Server
-RobotServer server("HermaniPhone", "vanskelig", pid, stateMachine);
+//RobotServer server("HermaniPhone", "vanskelig", pid, stateMachine);
 
 void setup() {
     Serial.begin(115200);
 
     // Setting PID values P, I, D
-    pid.setPID(15, 0, 30);
+    //pid.setPID(18, 0, 36);
+    //pid.setPID(21, 0, 46);
+    pid.setPID(27, 0, 30);
     pid.setAggressivePID(20, 0, 0);
 
     // Setting turn speed values
-    stateMachine.setTurnSpeed(-150, -40, -40, -150);
+    //stateMachine.setTurnSpeed(-40, -120, -120, -40);
+    stateMachine.setTurnSpeed(100, -120, -120, 100);
 
     // Setup
     button.setup();
-    server.setup();
 
     motorA.setup();
     motorB.setup();
@@ -50,7 +52,6 @@ void setup() {
 }
 
 void loop() {
-    server.update();
 
     uint16_t position = sensor.getQTRSensor()->readLineBlack(sensorValues);
     stateMachine.state(position, button.readState());
